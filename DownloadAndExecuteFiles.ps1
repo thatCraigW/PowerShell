43c14487805d22687f899902b1d1df29bd29528b
Craig W <44760431+thatCraigW@users.noreply.github.com>
2018-11-05 15:22:58 +1000

    Create DownloadAndExecuteFiles.ps1

---
+++ DownloadAndExecuteFiles.ps1
@@ -0,0 +1,10 @@
+wget -outf filename.extension URLtodownloadfrom
+.\filename.extension /silent
+
+# Live example & then run it quietly in the background
+
+
+<#
+wget -outf ninite.exe https://ninite.com/7zip-vlc/ninite.exe 
+.\ninite.exe /silent
+#>
