428d0143cdfe923aa6289792f5744377f344d037
thatCraigW <44760431+thatCraigW@users.noreply.github.com>
2018-11-05 13:01:05 +1000

    Add files via upload

---
+++ O365_Connect_PS.ps1
@@ -0,0 +1,3 @@
+$cred = Get-Credential
+$o365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $cred -Authentication Basic -AllowRedirection
+Import-PSSession $o365 -DisableNameChecking
