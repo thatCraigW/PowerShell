a50fd693cb8d937ac85520283ec8f8657767cd64
CraigW <craig.warren@outlook.com>
2018-11-05 21:22:20 +1000

    Typo

--- Office365/Convert_MailboxToShared.ps1
+++ Office365/Convert_MailboxToShared.ps1
@@ -1,6 +1,3 @@
-$cred = Get-Credential
-$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $cred -Authentication Basic -AllowRedirection
-Import-PSSession $session
 $user = Read-Host "Email Address to Convert to Shared Mailbox"
 Get-Mailbox -Identity $user | Format-List RecipientTypeDetails
 Set-Mailbox -Identity $user -Type Shared
