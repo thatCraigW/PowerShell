7ab001e8f314e0ba06266de3a0a88f7ff9c72209
Craig W <44760431+thatCraigW@users.noreply.github.com>
2019-11-04 10:35:38 +1000

    New Version

---
+++ Office365/Connect Office365.ps1
@@ -0,0 +1,486 @@
+function Connect-Office365{
+<#
+	.NOTES
+		===========================================================================
+		Created on:   	2/4/2019 10:42 PM
+		Created by:   	Bradley Wyatt
+		E-Mail:			Brad@TheLazyAdministrator.com
+		GitHub:			https://github.com/bwya77
+		Website:		https://www.thelazyadministrator.com
+		Organization: 	Porcaro Stolarek Mete Partners; The Lazy Administrator
+		Filename:     	Connect-Office365.ps1
+		Version: 		1.0.5
+	
+		Contributors:   /u/Sheppard_Ra
+	
+		Changelog:
+			1.0.5
+				- Updated comment based help
+			1.0.4
+				- Host title will add a service or services you are connected to. If unable to connect it will not display connection status until connection is valid
+		===========================================================================
+
+    .SYNOPSIS
+        Connect to Office 365 Services
+
+    .DESCRIPTION
+        Connect to different Office 365 Services using PowerShell function. Supports MFA.
+
+    .PARAMETER MFA
+		Description: Specifies MFA requirement to sign into Office 365 services. If set to $True it will use the Office 365 ExoPSSession Module to sign into Exchange & Compliance Center using MFA. Other modules support MFA without needing another external module
+
+    .PARAMETER Service
+		Description: Specify service to connect to (Exchange, AzureAD, MSOnline, Teams, SecurityandCompliance, SharePoint, SkypeForBusiness)
+
+    .EXAMPLE
+		Description: Connect to SharePoint Online
+        C:\PS> Connect-Office365 -SharePoint
+
+    .EXAMPLE
+		Description: Connect to Exchange Online and Azure AD V1 (MSOnline)
+        C:\PS> Connect-Office365 -Service Exchange, MSOnline
+
+    .EXAMPLE
+		Description: Connect to Exchange Online and Azure AD V2 using Multi-Factor Authentication
+        C:\PS> Connect-Office365 -Service Exchange, MSOnline -MFA
+
+	.EXAMPLE
+		Description: Connect to Teams and Skype for Business
+        C:\PS> Connect-Office365 -Service Teams, SkypeForBusiness
+	
+	.EXAMPLE
+		Description: Connect to SharePoint Online
+		 C:\PS> Connect-Office365 -Service SharePoint -SharePointOrganizationName bwya77 -MFA
+
+    .LINK
+        Online version:  https://www.thelazyadministrator.com/2019/02/05/powershell-function-to-connect-to-all-office-365-services
+
+#>
+	
+	[OutputType()]
+	[CmdletBinding(DefaultParameterSetName)]
+	Param (
+		[Parameter(Mandatory = $True, Position = 1)]
+		[ValidateSet('AzureAD', 'Exchange', 'MSOnline', 'SecurityAndCompliance', 'SharePoint', 'SkypeForBusiness', 'Teams')]
+		[string[]]$Service,
+		[Parameter(Mandatory = $False, Position = 2)]
+		[Alias('SPOrgName')]
+		[string]$SharePointOrganizationName,
+		[Parameter(Mandatory = $False, Position = 3, ParameterSetName = 'Credential')]
+		[PSCredential]$Credential,
+		[Parameter(Mandatory = $False, Position = 3, ParameterSetName = 'MFA')]
+		[Switch]$MFA
+	)
+	
+	$getModuleSplat = @{
+		ListAvailable = $True
+		Verbose	      = $False
+	}
+	
+	If ($MFA -ne $True)
+	{
+		Write-Verbose "Gathering PSCredentials object for non MFA sign on"
+		$Credential = Get-Credential -Message "Please enter your Office 365 credentials"
+	}
+	
+	ForEach ($Item in $PSBoundParameters.Service)
+	{
+		Write-Verbose "Attempting connection to $Item"
+		Switch ($Item)
+		{
+			AzureAD {
+				If ($null -eq (Get-Module @getModuleSplat -Name "AzureAD"))
+				{
+					Write-Error "AzureAD Module is not present!"
+					continue
+				}
+				Else
+				{
+					If ($MFA -eq $True)
+					{
+						$Connect = Connect-AzureAD
+						If ($null -ne $Connect)
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: AzureAD"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - AzureAD"
+							}
+						}
+						
+					}
+					Else
+					{
+						$Connect = Connect-AzureAD -Credential $Credential
+						If ($Null -ne $Connect)
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: AzureAD"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - AzureAD"
+							}
+						}
+					}
+				}
+				continue
+			}
+			
+			Exchange {
+				If ($MFA -eq $True)
+				{
+					$getChildItemSplat = @{
+						Path = "$Env:LOCALAPPDATA\Apps\2.0\*\CreateExoPSSession.ps1"
+						Recurse = $true
+						ErrorAction = 'SilentlyContinue'
+						Verbose = $false
+					}
+					$MFAExchangeModule = ((Get-ChildItem @getChildItemSplat | Select-Object -ExpandProperty Target -First 1).Replace("CreateExoPSSession.ps1", ""))
+					
+					If ($null -eq $MFAExchangeModule)
+					{
+						Write-Error "The Exchange Online MFA Module was not found!
+        https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/mfa-connect-to-exchange-online-powershell?view=exchange-ps"
+						continue
+					}
+					Else
+					{
+						Write-Verbose "Importing Exchange MFA Module"
+						. "$MFAExchangeModule\CreateExoPSSession.ps1"
+						
+						Write-Verbose "Connecting to Exchange Online"
+						Connect-EXOPSSession
+						If ($Null -ne (Get-PSSession | Where-Object { $_.ConfigurationName -like "*Exchange*" }))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Exchange"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Exchange"
+							}
+						}
+					}
+				}
+				Else
+				{
+					$newPSSessionSplat = @{
+						ConfigurationName = 'Microsoft.Exchange'
+						ConnectionUri	  = "https://ps.outlook.com/powershell/"
+						Authentication    = 'Basic'
+						Credential	      = $Credential
+						AllowRedirection  = $true
+					}
+					$Session = New-PSSession @newPSSessionSplat
+					Write-Verbose "Connecting to Exchange Online"
+					Import-PSSession $Session -AllowClobber
+					If ($Null -ne (Get-PSSession | Where-Object { $_.ConfigurationName -like "*Exchange*" }))
+					{
+						If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+						{
+							$host.ui.RawUI.WindowTitle += " - Connected To: Exchange"
+						}
+						Else
+						{
+							$host.ui.RawUI.WindowTitle += " - Exchange"
+						}
+					}
+					
+				}
+				continue
+			}
+			
+			MSOnline {
+				If ($null -eq (Get-Module @getModuleSplat -Name "MSOnline"))
+				{
+					Write-Error "MSOnline Module is not present!"
+					continue
+				}
+				Else
+				{
+					Write-Verbose "Connecting to MSOnline"
+					If ($MFA -eq $True)
+					{
+						Connect-MsolService
+						If ($Null -ne (Get-MsolCompanyInformation -ErrorAction SilentlyContinue))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: MSOnline"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - MSOnline"
+							}
+						}
+					}
+					Else
+					{
+						Connect-MsolService -Credential $Credential
+						If ($Null -ne (Get-MsolCompanyInformation -ErrorAction SilentlyContinue))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: MSOnline"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - MSOnline"
+							}
+						}
+					}
+				}
+				continue
+			}
+			
+			SecurityAndCompliance {
+				If ($MFA -eq $True)
+				{
+					$getChildItemSplat = @{
+						Path = "$Env:LOCALAPPDATA\Apps\2.0\*\CreateExoPSSession.ps1"
+						Recurse = $true
+						ErrorAction = 'SilentlyContinue'
+						Verbose = $false
+					}
+					$MFAExchangeModule = ((Get-ChildItem @getChildItemSplat | Select-Object -ExpandProperty Target -First 1).Replace("CreateExoPSSession.ps1", ""))
+					If ($null -eq $MFAExchangeModule)
+					{
+						Write-Error "The Exchange Online MFA Module was not found!
+        https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/mfa-connect-to-exchange-online-powershell?view=exchange-ps"
+						continue
+					}
+					Else
+					{
+						Write-Verbose "Importing Exchange MFA Module (Required)"
+						. "$MFAExchangeModule\CreateExoPSSession.ps1"
+						
+						Write-Verbose "Connecting to Security and Compliance Center"
+						Connect-IPPSSession
+						If ($Null -ne (Get-PSSession | Where-Object { $_.ConfigurationName -like "*Exchange*" }))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Security and Compliance Center"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Security and Compliance Center"
+							}
+						}
+					}
+				}
+				Else
+				{
+					$newPSSessionSplat = @{
+						ConfigurationName = 'Microsoft.SecurityAndCompliance'
+						ConnectionUri	  = 'https://ps.compliance.protection.outlook.com/powershell-liveid/'
+						Authentication    = 'Basic'
+						Credential	      = $Credential
+						AllowRedirection  = $true
+					}
+					$Session = New-PSSession @newPSSessionSplat
+					Write-Verbose "Connecting to SecurityAndCompliance"
+					Import-PSSession $Session -DisableNameChecking
+					If ($Null -ne (Get-PSSession | Where-Object { $_.ConfigurationName -like "*Exchange*" }))
+					{
+						If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+						{
+							$host.ui.RawUI.WindowTitle += " - Connected To: Security and Compliance Center"
+						}
+						Else
+						{
+							$host.ui.RawUI.WindowTitle += " - Security and Compliance Center"
+						}
+					}
+				}
+				continue
+			}
+			
+			SharePoint {
+				If ($null -eq (Get-Module @getModuleSplat -Name Microsoft.Online.SharePoint.PowerShell))
+				{
+					Write-Error "Microsoft.Online.SharePoint.PowerShell Module is not present!"
+					continue
+				}
+				Else
+				{
+					If (-not ($PSBoundParameters.ContainsKey('SharePointOrganizationName')))
+					{
+						Write-Error 'Please provide a valid SharePoint organization name with the -SharePointOrganizationName parameter.'
+						continue
+					}
+					
+					$SharePointURL = "https://{0}-admin.sharepoint.com" -f $SharePointOrganizationName
+					Write-Verbose "Connecting to SharePoint at $SharePointURL"
+					If ($MFA -eq $True)
+					{
+						Connect-SPOService -Url $SharePointURL
+						If ($Null -ne (Get-SPOTenant))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: SharePoint Online"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - SharePoint Online"
+							}
+						}
+					}
+					Else
+					{
+						Connect-SPOService -Url $SharePointURL -Credential $Credential
+						If ($Null -ne (Get-SPOTenant))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: SharePoint Online"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - SharePoint Online"
+							}
+						}
+					}
+				}
+				continue
+			}
+			
+			SkypeForBusiness {
+				Write-Verbose "Connecting to SkypeForBusiness"
+				If ($null -eq (Get-Module @getModuleSplat -Name "SkypeOnlineConnector"))
+				{
+					Write-Error "SkypeOnlineConnector Module is not present!"
+				}
+				Else
+				{
+					# Skype for Business module
+					Import-Module SkypeOnlineConnector
+					If ($MFA -eq $True)
+					{
+						$CSSession = New-CsOnlineSession
+						Import-PSSession $CSSession -AllowClobber
+						If ($Null -ne (Get-CsOnlineDirectoryTenant))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Skype for Business"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Skype for Business"
+							}
+						}
+					}
+					Else
+					{
+						$CSSession = New-CsOnlineSession -Credential $Credential
+						Import-PSSession $CSSession -AllowClobber
+						If ($Null -ne (Get-CsOnlineDirectoryTenant))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Skype for Business"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Skype for Business"
+							}
+						}
+					}
+				}
+				continue
+			}
+			
+			Teams {
+				If ($null -eq (Get-Module @getModuleSplat -Name "MicrosoftTeams"))
+				{
+					Write-Error "MicrosoftTeams Module is not present!"
+				}
+				Else
+				{
+					Write-Verbose "Connecting to Teams"
+					If ($MFA -eq $True)
+					{
+						$TeamsConnect = Connect-MicrosoftTeams
+						If ($Null -ne ($TeamsConnect))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Microsoft Teams"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Microsoft Teams"
+							}
+						}
+					}
+					Else
+					{
+						$TeamsConnect = Connect-MicrosoftTeams -Credential $Credential
+						If ($Null -ne ($TeamsConnect))
+						{
+							If (($host.ui.RawUI.WindowTitle) -notlike "*Connected To:*")
+							{
+								$host.ui.RawUI.WindowTitle += " - Connected To: Microsoft Teams"
+							}
+							Else
+							{
+								$host.ui.RawUI.WindowTitle += " - Microsoft Teams"
+							}
+						}
+					}
+				}
+				continue
+			}
+			Default { }
+		}
+	}
+}
+
+# Connect Everything
+#Connect-Office365 -Service Exchange, AzureAD, MSOnline, Teams, SecurityAndCompliance, SharePoint, SkypeForBusiness
+
+# Azure commands only
+#Connect-Office365 -Service AzureAD -MFA
+
+# 365 Phone System commands only
+#Connect-Office365 -Service Teams, SkypeForBusiness, MSOnline #-MFA
+
+# MSOL commands only
+Connect-Office365 -Service MSOnline #-MFA
+
+# Mailbox commands only
+#Connect-Office365 -Service Exchange #-MFA
+
+# Connect the basics / BAU
+#Connect-Office365 -Service Exchange, AzureAD, MSOnline #-MFA
+
+# New User Creation commands only
+#Connect-Office365 -Service Exchange, MSOnline -MFA
+
+# User Exit / Archive / Lock-out Commands;
+
+# Clear Immutable IDs to break sync (after deleting/disabling in AD and syncing)
+#$immutableUser = "danielle.schofield@sparrow.edu.au"
+#Set-MSOLUser -UserPrincipalName $immutableUser -ImmutableID "$null"
+#Get-MsolUser -UserPrincipalName $immutableUser | Select UserPrincipalName, ImmutableID
+
+# SharePoint commands only - REPLACE OrgName with the prefix from OrgName.sharepoint.com
+#Connect-Office365 -Service AzureAD, SharePoint -SharePointOrganizationName OrgName -MFA
+
+# Sign-out 365 services (Requires Sharepoint connection above)
+#Revoke-SPOUserSession - User email@domain.com -Confirm:$false
+
+# Sign-out Azure services (Requires Azure connection above)
+#Get-AzureADUser -searchstring email@domain.com | Revoke-AzureADUserAllRefreshToken
+
+# Set a password to never expire - ONLY WORKS ON NON-SYNCED ACCOUNTS (obviously, but just gotta say to remind you)
+# https://docs.microsoft.com/en-us/office365/admin/add-users/set-password-to-never-expire?view=o365-worldwide for more info I guess
+#Set-AzureADUser -ObjectId email@domain.com -PasswordPolicies DisablePasswordExpiration
